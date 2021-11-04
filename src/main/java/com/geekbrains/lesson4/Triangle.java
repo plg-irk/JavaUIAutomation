781e28b0a69a1584c474eb1ccf2a88a5c543d0aa
plg-irk <nk-irk@mail.ru>
2021-11-04 21:50:56 +0800

    Lesson4 Test example 1

--- src/main/java/com/geekbrains/lesson4/Triangle.java
+++ src/main/java/com/geekbrains/lesson4/Triangle.java
@@ -1,7 +1,10 @@
 package com.geekbrains.lesson4;
 
 public class Triangle {
-    public static void main(String[] args) {
 
+    public static double sumNumbers(int a, int b, int c) {
+        double p = (a + b + c) / 2;
+        double s = Math.pow(p * (p - a) * (p - b) * (p - c), 1.0 / 2);
+        return s;
     }
 }
